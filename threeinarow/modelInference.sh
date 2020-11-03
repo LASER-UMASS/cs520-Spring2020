629c1278b9cb369bb7e1b8d61bc2471e32dcf7c5
Heather M Conboy <hconboy@cs.umass.edu>
2020-11-03 15:21:07 -0500

    Simplified the modelInference script to always take as input every trace file in the traces directory

--- threeinarow/modelInference.sh
+++ threeinarow/modelInference.sh
@@ -3,4 +3,4 @@
 # Runs Synoptic from the compiled JAR file, passing all command
 # line argument directly to main().
 
-java -ea -jar ./lib/synoptic/lib/synoptic.jar $* -o rowGameApp-arch
+java -ea -jar ./lib/synoptic/lib/synoptic.jar traces/trace-* -o rowGameApp-arch
